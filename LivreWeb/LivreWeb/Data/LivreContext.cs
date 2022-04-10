2fe13482395c99a3d5dbcb4469434ecbc6ddd878
Sinan <ergun.sinan31@gmail.com>
2022-04-10 16:43:59 +0200

    Initialisation Projet

---
+++ LivreWeb/LivreWeb/Data/LivreContext.cs
@@ -0,0 +1,13 @@
+﻿using LivreWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LivreWeb.Data
+{
+    public class LivreContext : DbContext
+    {
+        public DbSet<Categorie> Categories { get; set; }
+        public LivreContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ LivreWeb/LivreWeb/Models/Categorie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LivreWeb.Models
+{
+    public class Categorie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public int NombreCommandes { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+
+    }
+}
